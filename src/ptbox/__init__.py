718f7cab57dc8e258bd06d6d2d1d0ef8252a6d9d
daisuke0530 <daisuke0530@gmail.com>
2020-08-31 16:10:07 +0900

    fix init

--- src/ptbox/__init__.py
+++ src/ptbox/__init__.py
@@ -1,5 +1,5 @@
 from .registry import (
-    MODELS, LOSSES, OPTIMIZERS, SCHEDULERS, DATASETS, CALLBACKS, METRICS,
+    MODELS, LOSSES, OPTIMIZERS, SCHEDULERS, DATASETS, CALLBACKS, METRICS, TRANSFORMS
     build_from_config,
     build_from_config_list, 
     build_from_config_dict
